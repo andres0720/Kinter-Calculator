2930e26acffe3ce237887c55943b5fddc2e8b7fc
Andres Bedoya <andres0720B@gmail.com>
2023-10-03 20:14:48 -0400

    Here's the basic layout of my calculator app

---
+++ KinterCalc.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("kinterCalc")
+
+#Define the Entry box 
+e = tk.Entry(root, width = 35,borderwidth=5)
+e.grid(row=0,column=0, columnspan=3,  padx=10,pady=10)
+
+def button_click(number):
+    e.delete(0, tk.END)
+    e.insert(0, number)
+    
+    
+ # Define Buttons
+button1= tk.Button(root, text='1', padx=40, pady=20, command=lambda: button_click(1))
+button2= tk.Button(root, text='2', padx=40, pady=20, command=lambda: button_click(2))
+button3= tk.Button(root, text='3', padx=40, pady=20, command=lambda: button_click(3))
+button4= tk.Button(root, text='4', padx=40, pady=20, command=lambda: button_click(4))
+button5= tk.Button(root, text='5', padx=40, pady=20, command=lambda: button_click(5))
+button6= tk.Button(root, text='6', padx=40, pady=20, command=lambda: button_click(6))
+button7= tk.Button(root, text='7', padx=40, pady=20, command=lambda: button_click(7))
+button8= tk.Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8))
+button9= tk.Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9))
+button0= tk.Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0))
+
+button_add= tk.Button(root, text='+', padx=39, pady=20, command=button_click)
+button_equal= tk.Button(root, text='=', padx=86.9, pady=20, command=button_click)
+button_clear= tk.Button(root, text='Clear', padx=77, pady=20, command=button_click)
+
+
+#put buttons on screen 
+button1.grid(row=3, column=0)
+button2.grid(row=3, column=1)
+button3.grid(row=3, column=2)
+
+button4.grid(row=2, column=0)
+button5.grid(row=2, column=1)
+button6.grid(row=2, column=2)
+
+button7.grid(row=1, column=0)
+button8.grid(row=1, column=1)
+button9.grid(row=1, column=2)
+
+button0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
